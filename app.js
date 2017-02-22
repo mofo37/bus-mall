33e66bdb99eec3d701b86eb4e3a7ce00968e5078
Morgan Fogarty <mofo37@gmail.com>
2017-02-21 16:36:24 -0800

    accessed array object properties to display images

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 'use strict';
 
 
-var images = ['images/bag.jpg', 'images/banana.jpg', 'images/bathroom.jpg', 'images/boots.jpg', 'images/breakfast.jpg', 'images/bubblegum.jpg', 'images/chair.jpg', 'images/cthulhu.jpg', 'images/dog-duck.jpg', 'images/dragon.jpg', 'images/pen.jpg', 'images/pet-sweep.jpg', 'images/scissors.jpg', 'images/shark.jpg', 'images/sweep.png', 'images/tauntaun.jpg', 'images/unicorn.jpg', 'images/usb.gif', 'images/water-can.jpg', 'images/wine-glass.jpg'];
-
-
 function Display (imageName, path) {
   this.displayed = 0;
   this.clicked = 0;
@@ -11,19 +8,38 @@ function Display (imageName, path) {
   this.name = imageName;
 }
 
+var bag = new Display('bag', 'bag.jpg');
+var banana = new Display('banana', 'banana.jpg');
+var bathroom = new Display('bathroom', 'bathroom.jpg');
+var boots = new Display('boots', 'boots.jpg');
+var breakfast = new Display('breakfast', 'breakfast.jpg');
+var bubblegum = new Display('bubblegum', 'bubblegum.jpg');
+var chair = new Display('chair', 'chair.jpg');
+var cthulhu = new Display('cthulhu', 'cthulhu.jpg');
+var dogDuck = new Display('dogDuck', 'dog-duck.jpg');
+var dragon = new Display('dragon', 'dragon.jpg');
+var pen = new Display('pen', 'pen.jpg');
+var petSweep = new Display('petSweep', 'pet-sweep.jpg');
+var scissors = new Display('scissors', 'scissors.jpg');
+var shark = new Display('shark', 'shark.jpg');
+var sweep = new Display('sweep', 'sweep.png');
+var tauntaun = new Display('tauntaun', 'tauntaun.jpg');
+var unicorn = new Display('unicorn', 'unicorn.jpg');
+var usb = new Display('usb', 'usb.gif');
+var waterCan = new Display('waterCan', 'water-can.jpg');
+var wineGlass = new Display('wineGlass', 'wine-glass.jpg');
 
-Display.prototype.totalClicksPerImage = function() {
-  for (var i = 0; i < 25; i++);
-
-};
+var images = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
 
 var randomNumber = function() {
   return Math.floor(Math.random() * (images.length));
 };
 
 var currentImages = function() {
-  return [images[randomNumber()], images[randomNumber()], images[randomNumber()]];
+  return [[images[randomNumber()].filepath], [images[randomNumber()].filepath], [images[randomNumber()].filepath]];
 };
+
+console.log(currentImages());
 
 var pageSwitch = function() {
   document.getElementById('first').setAttribute('src', currentImages()[0]);
@@ -35,26 +51,8 @@ document.getElementById('first').addEventListener('click', pageSwitch);
 document.getElementById('second').addEventListener('click', pageSwitch);
 document.getElementById('third').addEventListener('click', pageSwitch);
 
-
-var bag = new Display('bag', 'bag.jpg');
-var banana = new Display('banana', 'banana.jpg');
-var bathroom = new Display('bathroom', 'bathroom.jpg');
-var boots = new Display('boots', 'boots.jpg');
-var breakfast = new Display('breakfast', 'breakfast.jpg');
-var bubblegum = new Display('bubblegum', 'bubblegum.jpg');
-var chair = new Display('chair', 'chair.jpg');
-var cthulhu = new Display('cthulu', 'cthulu.jpg');
-var dogDuck = new Display('dogDuck', 'dogDuck.jpg');
-var dragon = new Display('dragon', 'dragon.jpg');
-var pen = new Display('pen', 'pen.jpg');
-var petSweep = new Display('petSweep', 'petSweep.jpg');
-var scissors = new Display('scissors', 'scissors.jpg');
-var shark = new Display('shark', 'shark.jpg');
-var sweep = new Display('sweep', 'sweep.png');
-var tauntaun = new Display('tauntaun', 'tauntaun.jpg');
-var unicorn = new Display('unicorn', 'unicorn.jpg');
-var usb = new Display('usb', 'usb.gif');
-var waterCan = new Display('waterCan', 'water-can.jpg');
-var wineGlass = new Display('wineGlass', 'wine-glass.jpg');
+// for (i = 0; i < images.length; i++) {
+//   images[randomNumber(i)[1].filepath];
+// };
 
 pageSwitch();
