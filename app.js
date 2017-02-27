e5e5ff1c2abc3d038cbdbd86bb07446071f68d1d
Morgan Fogarty <mofo37@gmail.com>
2017-02-27 00:41:18 -0800

    organized js with function declarations first.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var stringImages = localStorage.getItem('storageImages');
-var storageImages1 = JSON.parse(stringImages);
+// var stringImages = localStorage.getItem('storageImages');
+// var storageImages1 = JSON.parse(stringImages);
 
 
 function Display (imageName, path) {
@@ -10,6 +10,107 @@ function Display (imageName, path) {
   this.filepath = 'images/' + path;
   this.name = imageName;
 }
+
+var randomNumber = function(arr) {
+  return Math.floor(Math.random() * (arr.length));
+};
+
+var nonDuplicate = function() {
+  var adjustedArray = [];
+  for (var i = 0; i < images.length; i++) {
+
+    if (displayedImages.includes(images[i])) {
+    } else {
+      adjustedArray.push(images[i]);
+    }
+  };
+
+  var outputArray = [];
+  for (var j = 0; j < 3; j++) {
+    var imageIndex = randomNumber(adjustedArray);
+    outputArray.push(adjustedArray[imageIndex]);
+    adjustedArray.splice(imageIndex, 1);
+  };
+
+  return outputArray;
+};
+
+var clickCounter = function(clickedImage) {
+  var id = clickedImage.getAttribute('id');
+
+  var image = '';
+
+  if (id === 'first') {
+    image = displayedImages[0];
+  } else if (id === 'second') {
+    image = displayedImages[1];
+  } else {
+    image = displayedImages[2];
+  }
+
+  image.clicked++;
+};
+
+var pageSwitch = function() {
+  var imageTrio = nonDuplicate();
+  displayedImages = imageTrio;
+
+  for (var i = 0; i < imageTrio.length; i++) {
+    imageTrio[i].displayed++;
+  };
+
+  // var storageImages = images;
+  // JSON.stringify(storageImages);
+  // localStorage.setItem('storageImages', JSON.stringify(storageImages));
+
+  document.getElementById('first').setAttribute('src', imageTrio[0].filepath);
+  document.getElementById('second').setAttribute('src', imageTrio[1].filepath);
+  document.getElementById('third').setAttribute('src', imageTrio[2].filepath);
+};
+
+var clickReceiver = function() {
+  displayCount++;
+  clickCounter(this);
+  if (displayCount < 26) {
+    pageSwitch();
+  } else {
+    document.getElementById('images').removeChild(document.getElementById('first'));
+    document.getElementById('images').removeChild(document.getElementById('second'));
+    document.getElementById('images').removeChild(document.getElementById('third'));
+    showChart();
+  }
+};
+
+var showChart = function() {
+  var ctx = document.getElementById('myChart');
+
+  var chartConfig = {
+    type: 'bar',
+    data: {
+      labels: images.map(function(display){return display.name;}),
+      datasets: [{
+        label: 'Number of Clicks Per Image',
+        data: images.map(function(display){return display.clicked;}),
+        backgroundColor: ['red','blue','black','grey','red','blue','black','grey','red','blue','black','grey','red','blue','black','grey','red','blue','black','grey'],
+        borderColor: ['black'],
+        borderWidth: 1,
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  };
+
+  new Chart(ctx, chartConfig);
+};
+
+
 
 var bag = new Display('bag', 'bag.jpg');
 var banana = new Display('banana', 'banana.jpg');
@@ -33,112 +134,12 @@ var waterCan = new Display('waterCan', 'water-can.jpg');
 var wineGlass = new Display('wineGlass', 'wine-glass.jpg');
 
 var images = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
-
-var randomNumber = function(arr) {
-  return Math.floor(Math.random() * (arr.length));
-};
-
 var displayedImages = [];
 var displayCount = 0;
-
-var nonDuplicate = function() {
-  var adjustedArray = [];
-  for (var i = 0; i < images.length; i++) {
-
-    if (displayedImages.includes(images[i])) {
-    } else {
-      adjustedArray.push(images[i]);
-    }
-  };
-
-  var outputArray = [];
-  for (var j = 0; j < 3; j++) { 
-    var imageIndex = randomNumber(adjustedArray);
-    outputArray.push(adjustedArray[imageIndex]);
-    adjustedArray.splice(imageIndex, 1);
-  };
-
-  return outputArray;
-};
-
-
-var clickCounter = function(clickedImage) {
-  var id = clickedImage.getAttribute('id');
-
-  if (id === 'first') {
-    var image = displayedImages[0];
-  } else if (id === 'second') {
-    var image = displayedImages[1];
-  } else {
-    var image = displayedImages[2];
-  }
-
-  image.clicked++;
-};
-
-var pageSwitch = function() {
-  var imageTrio = nonDuplicate();
-  displayedImages = imageTrio;
-
-  for (var i = 0; i < imageTrio.length; i++) {
-    imageTrio[i].displayed++;
-  };
-
-  var storageImages = images;
-  JSON.stringify(storageImages);
-  localStorage.setItem('storageImages', JSON.stringify(storageImages));
-
-  document.getElementById('first').setAttribute('src', imageTrio[0].filepath);
-  document.getElementById('second').setAttribute('src', imageTrio[1].filepath);
-  document.getElementById('third').setAttribute('src', imageTrio[2].filepath);
-};
-
-var clickReceiver = function() {
-  displayCount++;
-  clickCounter(this);
-  if (displayCount < 26) {
-    pageSwitch();
-  } else {
-    document.getElementById('images').removeChild(document.getElementById('first'));
-    document.getElementById('images').removeChild(document.getElementById('second'));
-    document.getElementById('images').removeChild(document.getElementById('third'));
-    showChart();
-  }
-  console.log(displayCount);
-};
 
 document.getElementById('first').addEventListener('click', clickReceiver);
 document.getElementById('second').addEventListener('click', clickReceiver);
 document.getElementById('third').addEventListener('click', clickReceiver);
 
-
 pageSwitch();
 
-var showChart = function() {
-var ctx = document.getElementById('myChart');
-
-var chartConfig = {
-  type: 'bar',
-  data: {
-    labels: images.map(function(display){return display.name}),
-    datasets: [{
-      label: 'Number of Clicks Per Image',
-      data: images.map(function(display){return display.clicked}),
-      backgroundColor: ['red','blue','black','grey','red','blue','black','grey','red','blue','black','grey','red','blue','black','grey','red','blue','black','grey'],
-      borderColor: ['black'],
-      borderWidth: 1,
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-};
-
-var renderedChart = new Chart(ctx, chartConfig);
-};
